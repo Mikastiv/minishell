aaf53210e6e7441a18cd292e5d568e93d2aee116
mikastiv <m.leblanc_3@hotmail.com>
2021-10-17 02:19:03 -0400

    color change

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 00:42:47 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/10/17 02:12:53 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/10/17 02:18:04 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,14 @@ char	*get_prompt(void)
 	t_string	prompt;
 	char		*dir;
 
-	prompt = ft_str_new_copy("\001"FT_BOLD FT_BGRE"\002");
+	prompt = ft_str_new_copy("\001"FT_BOLD FT_BRED"\002");
 	if (g_mini.user)
 		ft_str_append_cstr(prompt, g_mini.user);
 	else
 		ft_str_append_cstr(prompt, "tty");
 	ft_str_append_cstr(prompt, "\001"FT_RST"\002:");
 	dir = getcwd_dir();
-	ft_str_append_cstr(prompt, "\001"FT_BOLD FT_BRED"\002");
+	ft_str_append_cstr(prompt, "\001"FT_BOLD FT_BBLU"\002");
 	ft_str_append_cstr(prompt, dir);
 	ft_str_append_cstr(prompt, "\001"FT_RST"\002$ ");
 	free(dir);
